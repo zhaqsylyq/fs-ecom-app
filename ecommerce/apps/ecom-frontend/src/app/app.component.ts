4c800807664ea9634c6f9dfe653caa68420d3c74
zhaqsylyq <zhaksylyk.chalgimbayev@nu.edu.kz>
2025-02-05 22:15:18 +0500

    UI architecture done

--- ecommerce/apps/ecom-frontend/src/app/app.component.ts
+++ ecommerce/apps/ecom-frontend/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {FaConfig, FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule, FaIconComponent, NavbarComponent, FooterComponent],
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecom-frontend';
+export class AppComponent implements OnInit{
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ ecommerce/apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,28 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import {
+  faCartShopping,
+  faCheck,
+  faShoppingCart,
+  faTruckFast,
+  faUser,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faTwitter,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faUser,
+  faCartShopping,
+  faTruckFast,
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faXmark,
+  faCheck,
+  faShoppingCart,
+  faCircleCheck,
+];
